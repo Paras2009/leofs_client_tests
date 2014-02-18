b712e3665e5c00399126c21caef2959c5274333e
Paras Patel <parasprince2001@gmail.com>
2014-02-18 20:05:01 +0900

    Updated ruby client and added new methods

--- aws-sdk-ruby/leo.rb
+++ aws-sdk-ruby/leo.rb
@@ -1,4 +1,4 @@
-# AWS::S3::Errors::NoSuchKey# This code supports "aws-sdk v1.9.5"
+## This code supports "aws-sdk v1.9.5"
 require "aws-sdk"
 
 Endpoint = "localhost"
@@ -6,7 +6,7 @@ Port = 8080
 # set your s3 key
 AccessKeyId = "05236"
 SecretAccessKey = "802562235"
-
+Bucket = "test"
 class LeoFSHandler < AWS::Core::Http::NetHttpHandler
   def handle(request, response)
     request.port = ::Port
@@ -32,66 +32,89 @@ AWS.config(
 
 s3 = AWS::S3.new
 
-begin
-    # PUT
-    # create bucket
-    s3.buckets.create("photo")
-    
-    # get bucket
-    bucket = s3.buckets["photo"]
-    
-    # create a new object
-    object = bucket.objects.create("image", "value")
-    
-    # show objects in the bucket
-    bucket.objects.with_prefix("").each do |obj|
-      p obj
-    end
-    
-    # retrieve an object
-    object = bucket.objects["image"]
-    
-    # insert an object
-    object.write(
-      file: "test.txt",
-      content_type: "text/plain"
-    )
-    
-    # GET
-    image = object.read
-    p image
-    
-    # HEAD
+# create bucket
+	#s3.buckets.create(Bucket)
+	print "Bucket Created Successfully \n"
+
+# get bucket
+	bucket = s3.buckets[Bucket]
+
+# create a new object
+	object = bucket.objects.create("image", "value")
+	print "Successfully created text file \n"
+# retrieve an object
+	print "Your object is :"
+	object = bucket.objects["image"]
+
+ # insert an object
+	object.write(
+		file: "test.txt",
+		content_type: "text/plain"
+	)
+# read image
+	image = object.read
+	p image
+	print "File MetaData\n"
+# HEAD
     metadata = object.head
     p metadata
-    
-    # DELETE
-    object.delete
 
-    # Multi part
+# Multi part
+    
     file_path_for_multipart_upload = '32M.dat'
+    print "File is being upload : \n "
     open(file_path_for_multipart_upload) do |file|
+      counter = file.size / 20242880
       uploading_object = bucket.objects[File.basename(file.path)]
       uploading_object.multipart_upload do |upload|
         while !file.eof?
-          upload.add_part(file.read 5242880) ## 5MB ##
+	  puts " #{upload.id} \t\t  #{counter} " 
+	  counter -= 1
+          upload.add_part(file.read 20242880) ## 20MB ##
           p('Aborted') if upload.aborted?
         end
       end
     end
+   print "File Uploaded Successfully \n"
     large_object = bucket.objects["32M.dat"]
-    image = large_object.read
-    
-    # GET(To be handled at the below rescure block)
+# HEAD    
+	metadata = large_object.head 
+	p metadata
+
+# GET(To be handled at the below rescure block)
     image = object.read
     p image
-rescue AWS::S3::Errors::NoSuchKey
-    exit
-rescue
-    # unexpected error occured
-    p $!
-    exit(-1)
-ensure
-    bucket = s3.buckets["photo"]
-    bucket.delete 
-end
+
+# Copy object
+	bucket.objects["32M.dat.copy"].copy_from("32M.dat") 
+	print "File copied successfully \n"
+
+# Rename object or move object
+        bucket.objects["image"].move_to("new_image");
+	#bucket.objects["image"].rename_to("new_image");
+	print "File rename or move Successfully \n"
+
+# show objects in the bucket
+	print "----------List Files--------- \n"
+	bucket.objects.with_prefix("").each do |obj|
+          puts "  #{obj.key} \t #{obj.content_length} " 
+	end
+
+#Download File
+	File.open('32M.dat.copy', 'wb') do |file|
+	bucket.objects['32M.dat'].read do |chunk|
+	    file.write(chunk)
+	  end
+	print "File Downloaded Successfully \n "	
+	end
+
+# Delete objects one by one
+	  print "--------------------Delete Files-------------------- \n"
+        bucket.objects.with_prefix("").each do |obj|
+		obj.delete
+        	print " #{obj.key} \t\t File Deleted Successfully.. \n" 
+        end
+       bucket.clear!  #clear the versions only
+
+#       bucket.delete
+       print "Bucket deleted Successfully \n"
