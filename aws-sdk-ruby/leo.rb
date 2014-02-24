81f1482fb50d4dd575228a069b0fe01852d41d70
Paras Patel <parasprince2001@gmail.com>
2014-02-24 17:27:23 +0900

    Updated leo.rb for indent change

--- aws-sdk-ruby/leo.rb
+++ aws-sdk-ruby/leo.rb
@@ -16,8 +16,8 @@ end
 
 SP = AWS::Core::CredentialProviders::StaticProvider.new(
 {
-    :access_key_id     => AccessKeyId,
-    :secret_access_key => SecretAccessKey
+  :access_key_id     => AccessKeyId,
+  :secret_access_key => SecretAccessKey
 })
 
 AWS.config(
@@ -33,65 +33,62 @@ AWS.config(
 s3 = AWS::S3.new
 
 begin
-    # PUT
-    # create bucket
-    s3.buckets.create("photo")
-    
-    # get bucket
-    bucket = s3.buckets["photo"]
-    
-    # create a new object
-    object = bucket.objects.create("image", "value")
-    
-    # show objects in the bucket
-    bucket.objects.with_prefix("").each do |obj|
-      p obj
-    end
-    
-    # retrieve an object
-    object = bucket.objects["image"]
-    
-    # insert an object
-    object.write(
-      file: "test.txt",
-      content_type: "text/plain"
-    )
-    
-    # GET
-    image = object.read
-    p image
-    
-    # HEAD
-    metadata = object.head
-    p metadata
-    
-    # DELETE
-    object.delete
+  # PUT
+  # create bucket
+  s3.buckets.create("test")
+  
+  # get bucket
+  bucket = s3.buckets["test"]
+  
+  # create a new object
+  object = bucket.objects.create("image", "value")
+  
+  # show objects in the bucket
+  bucket.objects.with_prefix("").each do |obj|
+    p obj
+  end
 
-    # Multi part
-    file_path_for_multipart_upload = '32M.dat'
-    open(file_path_for_multipart_upload) do |file|
-      uploading_object = bucket.objects[File.basename(file.path)]
-      uploading_object.multipart_upload do |upload|
-        while !file.eof?
-          upload.add_part(file.read 5242880) ## 5MB ##
-          p('Aborted') if upload.aborted?
-        end
+  # retrieve an object
+  object = bucket.objects["image"]
+
+  # insert an object
+  object.write( file: "test.txt",content_type: "text/plain")
+
+  # GET
+  image = object.read
+  p image
+
+  # HEAD
+  metadata = object.head
+  p metadata
+
+  # DELETE
+  object.delete
+
+  # Multi part
+  file_path_for_multipart_upload = '32M.dat'
+  open(file_path_for_multipart_upload) do |file|
+    uploading_object = bucket.objects[File.basename(file.path)]
+    uploading_object.multipart_upload do |upload|
+      while !file.eof?
+        upload.add_part(file.read 5242880) ## 5MB ##
+        p('Aborted') if upload.aborted?
       end
     end
-    large_object = bucket.objects["32M.dat"]
-    image = large_object.read
-    
-    # GET(To be handled at the below rescure block)
-    image = object.read
-    p image
+  end
+  large_object = bucket.objects["32M.dat"]
+  image = large_object.read
+
+  # GET(To be handled at the below rescure block)
+  image = object.read
+  p image
 rescue AWS::S3::Errors::NoSuchKey
-    exit
+  exit
 rescue
-    # unexpected error occured
-    p $!
-    exit(-1)
+  # unexpected error occured
+  p $!
+  exit(-1)
 ensure
-    bucket = s3.buckets["photo"]
-    bucket.delete 
+  bucket = s3.buckets["test"]
+  bucket.delete 
 end
