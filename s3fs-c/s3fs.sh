ad0ca773d0c4941d791f32e7cbffdb6ad98c4acc
Paras Patel <parasprince2001@gmail.com>
2014-03-13 11:42:50 +0900

    Update s3fs.sh

--- s3fs-c/s3fs.sh
+++ s3fs-c/s3fs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-cp -p README /mnt/s3fs-fuse && \
+s3cmd mb s3://test
+sudo -u jenkins /usr/local/s3fs-fuse/bin/s3fs test /mnt/s3fs-fuse -o allow_other,uid=498,gid=498,umask=0022 -o url='http://localhost:8080'
+cp -p ../temp_data/README /mnt/s3fs-fuse && \
 ls -l /mnt/s3fs-fuse && \
 cp  -p /mnt/s3fs-fuse/README README.copy && \
-diff README README.copy && rm README.copy && \
-cp README /mnt/s3fs-fuse/README.copy && \
+diff ../temp_data/README README.copy && rm README.copy && \
+cp /mnt/s3fs-fuse/README /mnt/s3fs-fuse/README.copy && \
+ls -l /mnt/s3fs-fuse && \
 mv /mnt/s3fs-fuse/README.copy /mnt/s3fs-fuse/README.org && \
-rm /mnt/s3fs-fuse/README /mnt/s3fs-fuse/README.org && \
-ls /mnt/s3fs-fuse
-/usr/bin/fusermount -u /mnt/s3fs-fuse
-
+ls -l /mnt/s3fs-fuse && \
+rm -f /mnt/s3fs-fuse/README /mnt/s3fs-fuse/README.org && \
+ls /mnt/s3fs-fuse && \
+umount  /mnt/s3fs-fuse
+s3cmd rb s3://test
