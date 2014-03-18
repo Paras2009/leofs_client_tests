6112a5183c786ac1eaa87d1f5be6d9a8d6e5f23b
Paras Patel <parasprince2001@gmail.com>
2014-03-18 11:58:30 +0900

    Update s3fs.sh For Code style changes

--- s3fs-c/s3fs.sh
+++ s3fs-c/s3fs.sh
@@ -2,15 +2,21 @@
 
 s3cmd mb s3://test
 sudo -u jenkins /usr/local/s3fs-fuse/bin/s3fs test /mnt/s3fs-fuse -o allow_other,uid=498,gid=498,umask=0022 -o url='http://localhost:8080'
+echo "Bucket Mounted Successfully" && \
 cp -p ../temp_data/README /mnt/s3fs-fuse && \
 ls -l /mnt/s3fs-fuse && \
+echo "File Uploaded to LeoFS" && \
 cp  -p /mnt/s3fs-fuse/README README.copy && \
+echo "File Downloaded Sucessfully" && \
 diff ../temp_data/README README.copy && rm README.copy && \
 cp /mnt/s3fs-fuse/README /mnt/s3fs-fuse/README.copy && \
 ls -l /mnt/s3fs-fuse && \
+echo "File coppied Successfully" && \
 mv /mnt/s3fs-fuse/README.copy /mnt/s3fs-fuse/README.org && \
 ls -l /mnt/s3fs-fuse && \
+echo "File Move or Rename Successfully" &&\
 rm -f /mnt/s3fs-fuse/README /mnt/s3fs-fuse/README.org && \
+echo "File Move or Rename Successfully" &&\
 ls /mnt/s3fs-fuse && \
 umount  /mnt/s3fs-fuse
 s3cmd rb s3://test
