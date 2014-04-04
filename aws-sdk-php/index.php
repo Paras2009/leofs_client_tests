a503218f7e27418b01ab26de9656d0a1ba4999ea
Paras Patel <parasprince2001@gmail.com>
2014-04-04 18:38:23 +0900

    Update index.php for common testFile

--- aws-sdk-php/index.php
+++ aws-sdk-php/index.php
@@ -22,21 +22,22 @@ try {
         print_r($bucket);
     }
 
+    // Create bucket
     print "Create New Bucket\n";
     $bucket_name="test";
-    // Create bucket
     $result = $client->createBucket(array(
         "Bucket" => $bucket_name
     ));
     print "Bucket Created Successfully \n";
 
-    print "Putting object into Bucket \n";
     // PUT object
+    print "Putting object into Bucket \n";
     $client->putObject(array( "Bucket" => $bucket_name, "Key" => "key-test", "Body" => "Hello, world!" ));
     print "Successfully created data file to testi \n";
 
-    print "Getting object from Bucket \n ";
+
     // GET object
+    print "Getting object from Bucket \n ";
     $object = $client->getObject(array( "Bucket" => $bucket_name, "Key" => "key-test" ));
     print($object->get("Body"));
     print "Head object\n " ;
@@ -47,19 +48,20 @@ try {
     print("File is uploading \n");
 
     // PUT file
-    $client->putObject(array( "Bucket" => $bucket_name, "Key" => "README", "Body" => fopen("../temp_data/README", "r") ));
+    $client->putObject(array( "Bucket" => $bucket_name, "Key" => "testFile", "Body" => fopen("../temp_data/testFile", "r") ));
     print("File Uploaded Successfully \n");
 
     // Download object file
-    $headers = $client->headObject(array( "Bucket" => $bucket_name, "Key" => "README" ));
+    $headers = $client->headObject(array( "Bucket" => $bucket_name, "Key" => "testFile" ));
     print_r($headers->toArray());
     print("\n\n");
+
     // GET object file
-    $object = $client->getObject(array( "Bucket" => $bucket_name, "Key" => "README", "SaveAs" => "README.copy" ));
+    $object = $client->getObject(array( "Bucket" => $bucket_name, "Key" => "testFile", "SaveAs" => "testFile.copy" ));
     print "File Successfully downloaded \n ";
 
     // Copy Object file
-    $result = $client->copyObject(array( "Bucket" => $bucket_name, "CopySource" => "/{$bucket_name}/README", "Key" => "README.copy",));
+    $result = $client->copyObject(array( "Bucket" => $bucket_name, "CopySource" => "/{$bucket_name}/testFile", "Key" => "testFile.copy",));
     print "File copied successfully \n";
 
     // List objects
@@ -70,7 +72,7 @@ try {
     }
 
     // DELETE object file
-    $client->deleteObject(array( "Bucket" => $bucket_name, "Key" => "README" ));
+    $client->deleteObject(array( "Bucket" => $bucket_name, "Key" => "testFile" ));
     print "File Deleted Successfully \n";
     print "delete bucket \n";
 
