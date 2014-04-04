8a4a4d5a2f057dda65e0f15b42d0a386aa49d7d5
Paras Patel <parasprince2001@gmail.com>
2014-04-04 11:19:36 +0900

    Update s3cmd client with code unification

--- s3cmd/s3cmd.sh
+++ s3cmd/s3cmd.sh
@@ -2,14 +2,19 @@
 
 
 which s3cmd && \
-    s3cmd mb s3://s3cmd-bucket && \
-    s3cmd put README s3://s3cmd-bucket/README && \
-    s3cmd ls s3://s3cmd-bucket && \
-    s3cmd get s3://s3cmd-bucket/README README.copy && \
-    diff README README.copy && rm README.copy && \
-    s3cmd cp s3://s3cmd-bucket/README s3://s3cmd-bucket/README.copy && \
-    s3cmd mv s3://s3cmd-bucket/README.copy s3://s3cmd-bucket/README.org && \
-    s3cmd del s3://s3cmd-bucket/README s3://s3cmd-bucket/README.org && \
-    s3cmd ls s3://s3cmd-bucket && \
-    s3cmd rb s3://s3cmd-bucket
-
+    s3cmd mb s3://test && \
+    echo "Bucket Created Successfully" && \
+    s3cmd put ../temp_data/testFile s3://test/testFile && \
+    echo "File Uploaded to LeoFS"
+    s3cmd ls s3://test && \
+    s3cmd get s3://test/testFile testFile.copy && \
+    echo "File Downloaded Sucessfully" && \
+    diff ../temp_data/testFile testFile.copy && rm testFile.copy && \
+    s3cmd cp s3://test/testFile s3://test/testFile.copy && \
+    echo "File coppied Successfully" && \
+    s3cmd mv s3://test/testFile.copy s3://test/testFile.org && \
+    echo "File Move or Rename Successfully" &&\
+    s3cmd del s3://test/testFile s3://test/testFile.org && \
+    echo "File Deleted Successfully" &&\
+    s3cmd ls s3://test && \
+    s3cmd rb s3://test 
