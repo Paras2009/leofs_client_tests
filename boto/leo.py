da70d21a31e16a4c07ee5ef874284a1035a11205
Paras Patel <parasprince2001@gmail.com>
2014-04-07 11:28:35 +0900

    Update leo.py with ACL test case & common testFile

--- boto/leo.py
+++ boto/leo.py
@@ -1,10 +1,11 @@
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 from boto.s3.bucket import Bucket
 from boto.s3.key import Key
+import time
 
 AWS_ACCESS_KEY = "05236"
 AWS_SECRET_ACCESS_KEY = "802562235"
-BUCKET_NAME = "test"
+BUCKET_NAME = "test" + time.strftime("%H%M%S")  #Dynamic BucketName
 
 conn = S3Connection(AWS_ACCESS_KEY,
     AWS_SECRET_ACCESS_KEY,
@@ -23,11 +24,72 @@ try:
     print "--------Bucket List------"
     for bucket in conn.get_all_buckets():
         print bucket
-    # bucket = conn.get_bucket(BUCKET_NAME)
+
+    # Get Bucket
+    bucket = conn.get_bucket(BUCKET_NAME,validate=False)
+    print "Get Bucket Successfully"
+
+    # Get-Put ACL
+    print "#####Default ACL#####"
+    acp = bucket.get_acl()
+    print acp
+    print "Owner ID : " + acp.owner.id
+    print "Owner Display name : " + acp.owner.display_name
+    permissions = []
+    for grant in acp.acl.grants:
+        print "Bucket ACL is :", grant.permission,"\nBucket Grantee URI is :", grant.uri
+        permissions.append(grant.permission)
+    if not all( x in permissions for x in ["FULL_CONTROL"]):
+        raise "Permission is Not full_control"
+    else:
+        print "Bucket ACL permission is  'private'.\n"
+
+    print "########:public_read ACL########"
+    bucket.set_acl("public-read")
+    acp = bucket.get_acl()
+    print "Owner ID :", acp.owner.id
+    print "Owner Display name :", acp.owner.display_name
+    permissions = []
+    for grant in acp.acl.grants:
+        print "Bucket ACL is :", grant.permission,"\nBucket Grantee URI is :", grant.uri
+        permissions.append(grant.permission)
+    if not all( x in permissions for x in ["READ","READ_ACP"]):
+         raise Exception("Permission is Not public_read")
+    else:
+        print "Bucket ACL Successfully changed to 'public-read'.\n"
+
+    print "#####:public_read_write ACL#####"
+    bucket.set_acl("public-read-write")
+    acp = bucket.get_acl()
+    print "Owner ID :", acp.owner.id
+    print "Owner Display name :", acp.owner.display_name
+    permissions = []
+    for grant in acp.acl.grants:
+        print "Bucket ACL is :", grant.permission,"\nBucket Grantee URI is :", grant.uri
+        permissions.append(grant.permission)
+    if not all( x in permissions for x in ["READ","READ_ACP","WRITE", "WRITE_ACP"]):
+        raise "Permission is Not public_read_write"
+    else:
+        print "Bucket ACL Successfully changed to 'public-read-write'.\n"
+
+    print "##########:private ACL##########"
+    bucket.set_acl("private")
+    acp = bucket.get_acl()
+    print "Owner ID :", acp.owner.id
+    print "Owner Display name :", acp.owner.display_name
+    permissions = []
+    for grant in acp.acl.grants:
+        print "Bucket ACL is :", grant.permission,"\nBucket Grantee URI is :", grant.uri
+        permissions.append(grant.permission)
+    if not all( x in permissions for x in ["FULL_CONTROL"]):
+        raise "Permission is Not full_control"
+    else:
+        print "Bucket ACL Successfully changed to 'private'.\n"
+
     # Create object
     s3_object = bucket.new_key("text")
     s3_object.set_contents_from_string("This is a text.")
-    print "Successfully created text file"
+    print "File Created Successfully"
 
     # Get Object
     s3_object = bucket.get_key("text")
@@ -39,16 +101,18 @@ try:
 
     # Write from file
     print "Uploading file.."
-    s3_object = bucket.new_key("README")
-    s3_object.set_contents_from_filename("../temp_data/README")
+    s3_object = bucket.new_key("testFile")
+    s3_object.set_contents_from_filename("../temp_data/testFile")
+
     # Print multipart_upload.upload()
     print "File uploaded Successfully"
 
     # File Download
-    s3_object.get_contents_to_filename("README.copy")
+    s3_object.get_contents_to_filename("testFile.copy")
     print "File Downloaded Successfully"
+
     # File copy
-    bucket.copy_key( "README.copy", BUCKET_NAME, "README" )
+    bucket.copy_key( "testFile.copy", BUCKET_NAME, "testFile" )
     print "File copied successfully"
 
     # Show Objects
@@ -62,11 +126,16 @@ try:
         print key.name , "Deleted Successfully", key.delete()
 
     # Get deleted key
-    s3_object = bucket.get_key("README")
+    s3_object = bucket.get_key("testFile")
+
     # It should print None
     print s3_object
+
+except Exception,e :
+    print e
+
 finally:
     # Delete Bucket
-    # bucket = conn.get_bucket(BUCKET_NAME)
+    bucket = conn.get_bucket(BUCKET_NAME,validate=False)
     bucket.delete()
     print "Bucket deleted Successfully \n"
