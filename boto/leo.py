0501406852ed583d6a27eda8ee624b82765fbc3d
Paras Patel <parasprince2001@gmail.com>
2014-04-11 10:51:48 +0900

    Update Python client with codding convention

--- boto/leo.py
+++ boto/leo.py
@@ -1,7 +1,6 @@
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 from boto.s3.bucket import Bucket
 from boto.s3.key import Key
-from cStringIO import StringIO
 import boto
 import hashlib
 import os
@@ -12,10 +11,10 @@ import magic
 
 AWS_ACCESS_KEY = "05236"
 AWS_SECRET_ACCESS_KEY = "802562235"
-FileName = "testFile"
-CHUNK_SIZE = 5 * 1024 * 1024
-mime = magic.open(magic.MAGIC_MIME)
 BUCKET_NAME = "test" + str(random.randint(1,99999))  ## Dynamic BucketName
+FILE_NAME = "testFile"
+CHUNK_SIZE = 5 * 1024 * 1024
+mimeType = magic.open(magic.MAGIC_MIME)
 
 conn = S3Connection(AWS_ACCESS_KEY,
     AWS_SECRET_ACCESS_KEY,
@@ -40,30 +39,29 @@ try:
     print "Get Bucket Successfully\n"
 
     # Put Object
-    file_path = "../temp_data/" + FileName
+    file_path = "../temp_data/" + FILE_NAME
     fileObject = open(file_path, "r")
-    fileDigest = hashlib.md5(fileObject.read()).hexdigest()
-    fileSize = os.path.getsize(file_path)
-    mime.load()
-    fileType =  mime.file(file_path)
+    file_digest = hashlib.md5(fileObject.read()).hexdigest()
+    file_size = os.path.getsize(file_path)
+    mimeType.load()
+    file_type = mimeType.file(file_path)
 
     # PUT Object using single-part method
-    bucket.new_key(FileName).set_contents_from_filename(file_path)
-
+    bucket.new_key(FILE_NAME).set_contents_from_filename(file_path)
     # HEAD Object
-    obj = bucket.get_key(FileName)
+    obj = bucket.get_key(FILE_NAME)
     if not(obj.exists()):
         raise "Object doesn't exists"
-    if not(fileSize == obj.size and fileDigest == obj.etag[1:-1]):
+    if not(file_size == obj.size and file_digest == obj.etag[1:-1]):
         raise "File Metadata could not match"
     else:
-        print "File MetaData : Content_type:", obj.content_type, "Content_encoding:", obj.content_encoding
-        print "etag:", obj.etag, "Size:", obj.size, "Name:", obj.name, "\n"
+        print "File MetaData : Content_type:", obj.content_type, "\b, Content_encoding:", obj.content_encoding
+        print "\b, etag:", obj.etag, "\b, Size:", obj.size, "\b, Name:", obj.name, "\n"
 
     # GET object
-    if not fileSize == obj.size:
+    if not file_size == obj.size:
         raise "Upload File content is not equal\n"
-    if "text/plain" in fileType:
+    if "text/plain" in file_type:
         print "Uploaded object data : \t", obj.read()
     else:
         print "File Content type is :", obj.content_type + "\n"
@@ -71,41 +69,41 @@ try:
     # Show Objects
     print"--------------------------------List Objects-----------------------------------"
     for key in bucket.list():
-        if not(fileSize == key.size):
+        if not(file_size == key.size):
             raise "Content length is changed for :", key.size
         print key.name, "\t\t", key.size, "\t\t", key.last_modified
     print "File Uploaded Successfully\n"
 
     # File copy
-    obj =  bucket.copy_key(FileName + ".copy", BUCKET_NAME, FileName)
+    obj = bucket.copy_key(FILE_NAME + ".copy", BUCKET_NAME, FILE_NAME)
     if not(obj.exists()):
        raise "File could not Copy Successfully\n"
 
     # Show Objects
     print"--------------------------------List Objects-----------------------------------"
     for key in bucket.list():
-        if not(fileSize == key.size):
+        if not(file_size == key.size):
             raise "Content length is changed for :", key.size
         print key.name, "\t\t", key.size, "\t\t", key.last_modified
     print "File copied successfully\n"
 
     # File Download
-    thisfile_path = FileName + ".copy"
-    obj.get_contents_to_filename(thisfile_path)
-    thisfileObject = open(thisfile_path, "r")
-    thisfileDigest = hashlib.md5(thisfileObject.read()).hexdigest()
-    thisfileSize = os.path.getsize(thisfile_path)
-    thisfileType = mime.file(thisfile_path)
-    if not(thisfileSize == obj.size and thisfileDigest == fileDigest):
+    this_file_path = FILE_NAME + ".copy"
+    obj.get_contents_to_filename(this_file_path)
+    thisfileObject = open(this_file_path, "r")
+    this_file_digest = hashlib.md5(thisfileObject.read()).hexdigest()
+    this_file_size = os.path.getsize(this_file_path)
+    thisfileType = mimeType.file(this_file_path)
+    if not(this_file_size == obj.size and this_file_digest == file_digest):
         raise "Downloaded File Metadata could not match\n"
     print "File Downloaded Successfully\n"
 
     # Delete objects one by one and check if exist
     print"--------------------------------Delete Objects---------------------------------"
     for key in bucket.list():
-        print key.name, "Deleted Successfully", key.delete()
+        print key.name, "\tDeleted Successfully", key.delete()
         if key.exists():
-            raise "Object is not Deleted Successfully\n"
+            raise "\tObject is not Deleted Successfully\n"
 
     # Get-Put ACL
     print "\n#####Default ACL#####"
@@ -120,7 +118,7 @@ try:
     if not all(x in permissions for x in ["FULL_CONTROL"]):
         raise "Permission is Not full_control"
     else:
-        print "Bucket ACL permission is  'private'\n"
+        print "Bucket ACL permission is 'private'\n"
 
     print "########:public_read ACL########"
     bucket.set_acl("public-read")
@@ -132,7 +130,7 @@ try:
         print "Bucket ACL is :", grant.permission,"\nBucket Grantee URI is :", grant.uri
         permissions.append(grant.permission)
     if not all(x in permissions for x in ["READ","READ_ACP"]):
-         raise Exception("Permission is Not public_read")
+        raise "Permission is Not public_read"
     else:
         print "Bucket ACL Successfully changed to 'public-read'\n"
 
