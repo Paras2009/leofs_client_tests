e1ca5485bd3086d054ebdae0d9598ad4fab937c1
Paras Patel <parasprince2001@gmail.com>
2014-04-11 12:41:28 +0900

    Update Python client

--- boto/leo.py
+++ boto/leo.py
@@ -14,7 +14,7 @@ AWS_SECRET_ACCESS_KEY = "802562235"
 BUCKET_NAME = "test" + str(random.randint(1,99999))  ## Dynamic BucketName
 FILE_NAME = "testFile"
 CHUNK_SIZE = 5 * 1024 * 1024
-mimeType = magic.open(magic.MAGIC_MIME)
+mime_type = magic.open(magic.MAGIC_MIME)
 
 conn = S3Connection(AWS_ACCESS_KEY,
     AWS_SECRET_ACCESS_KEY,
@@ -39,12 +39,13 @@ try:
     print "Get Bucket Successfully\n"
 
     # Put Object
+    print "File Upload Test [Start]\n"
     file_path = "../temp_data/" + FILE_NAME
-    fileObject = open(file_path, "r")
-    file_digest = hashlib.md5(fileObject.read()).hexdigest()
+    file_object = open(file_path, "r")
+    file_digest = hashlib.md5(file_object.read()).hexdigest()
     file_size = os.path.getsize(file_path)
-    mimeType.load()
-    file_type = mimeType.file(file_path)
+    mime_type.load()
+    file_type = mime_type.file(file_path)
 
     # Files in Amazon S3 are called "objects" and are stored in buckets. A specific object is 
     # referred to by its key (i.e., name) and holds data. Here, we create a new object with 
@@ -61,7 +62,8 @@ try:
         raise "Object doesn't exists"
     if not(file_size == obj.size and file_digest == obj.etag[1:-1]):
         raise "File Metadata could not match"
-    else:        print "File MetaData : Content_type:", obj.content_type, "\b, Content_encoding:", obj.content_encoding
+    else:
+        print "File MetaData : Content_type:", obj.content_type, "\b, Content_encoding:", obj.content_encoding
         print "\b, etag:", obj.etag, "\b, Size:", obj.size, "\b, Name:", obj.name, "\n"
 
     # GET object
@@ -79,8 +81,10 @@ try:
             raise "Content length is changed for :", key.size
         print key.name, "\t\t", key.size, "\t\t", key.last_modified
     print "File Uploaded Successfully\n"
+    print "File Upload Test [End]\n"
 
     # File copy
+    print "File Copy Test [Start]\n"
     obj = bucket.copy_key(FILE_NAME + ".copy", BUCKET_NAME, FILE_NAME)
     if not(obj.exists()):
        raise "File could not Copy Successfully\n"
@@ -92,26 +96,31 @@ try:
             raise "Content length is changed for :", key.size
         print key.name, "\t\t", key.size, "\t\t", key.last_modified
     print "File copied successfully\n"
+    print "File Copy Test [End]\n"
 
     # File Download
+    print "File Download Test [Start]\n"
     this_file_path = FILE_NAME + ".copy"
     obj.get_contents_to_filename(this_file_path)
-    thisfileObject = open(this_file_path, "r")
-    this_file_digest = hashlib.md5(thisfileObject.read()).hexdigest()
+    this_file_object = open(this_file_path, "r")
+    this_file_digest = hashlib.md5(this_file_object.read()).hexdigest()
     this_file_size = os.path.getsize(this_file_path)
-    thisfileType = mimeType.file(this_file_path)
     if not(this_file_size == obj.size and this_file_digest == file_digest):
         raise "Downloaded File Metadata could not match\n"
     print "File Downloaded Successfully\n"
+    print "File Download Test [End]\n"
 
     # Delete objects one by one and check if exist
-    print"--------------------------------Delete Objects---------------------------------"
+    print "File Delete Test [Start]\n"
+    print "--------------------------------Delete Objects---------------------------------"
     for key in bucket.list():
         print key.name, "\tDeleted Successfully", key.delete()
         if key.exists():
             raise "\tObject is not Deleted Successfully\n"
+    print "\nFile Delete Test [End]\n"
 
     # Get-Put ACL
+    print "Object ACL Test [Start]"
     print "\n#####Default ACL#####"
     acp = bucket.get_acl()
     print acp
@@ -167,6 +176,7 @@ try:
         raise "Permission is Not full_control"
     else:
         print "Bucket ACL Successfully changed to 'private'\n"
+    print "Bucket ACL Test [End]\n"
 except Exception,e :
     print traceback.format_exc()
 finally:
